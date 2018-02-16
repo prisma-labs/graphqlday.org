48bfa5c84c6628c7cfe0dd5ac5a8d3d9eae4c1f3
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-02-17 00:52:02 +0330

    feat: add a few more sections

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,40 @@
 import Footer from 'graphiconf/sections/Footer'
+import Speakers from 'graphiconf/sections/Speakers'
+import CoOrganizedBy from 'graphiconf/sections/CoOrganizedBy'
+import GetYourTicket from 'graphiconf/sections/GetYourTicket'
+import FullImage from 'components/FullImage'
 
 import SeoTitle from '../components/SeoTitle'
 import JoinUs from '../sections/JoinUs'
+import { speakersList } from '../utils/data'
 
 export default () => (
   <div>
     <SeoTitle />
 
     <JoinUs />
+    <Speakers
+      heading="Meet our GraphQL expert speakers"
+      speakersList={speakersList}
+    />
+
+    <FullImage src="/static/nemo-1.jpg" />
+    <GetYourTicket />
+
+    {/*
+        HERE!
+
+        I'm here
+
+        HERE!
+    */}
+
+    <FullImage src="/static/nemo-3.jpg" />
+
+    <div style={{ padding: '115px 0 30px 0' }}>
+      <CoOrganizedBy />
+    </div>
+
     <Footer />
   </div>
 )
---
+++ utils/data.js
@@ -0,0 +1,57 @@
+// Ken Wheeler - Director of OSS at FormidableLabs - https://twitter.com/ken_wheeler / https://github.com/kenwheeler
+// Kim Brandwijk - Author of SupergraphQL - https://github.com/kbrandwijk
+// Sara Vieira - Frontend Engineer at YLD.io - https://twitter.com/NikkitaFTW / https://github.com/SaraVieira
+// Martijn Walraven - Technical Product Manager at Apollo - https://twitter.com/martijnwalraven / https://github.com/martijnwalraven
+// Manjula Dabe - Senior Engineer at Bookmyshow - https://twitter.com/manjula_dube / https://github.com/manjula91
+// Ivan Goncharov - GraphQL & OpenAPI/Swagger consultant at APIsGuru - https://github.com/IvanGoncharov
+
+export const speakersList = [
+  {
+    imageUrl: '/static/speakers/speaker-1',
+    imageFormat: 'jpg',
+    name: 'Ken Wheeler',
+    bio: 'Director of OSS at FormidableLabs',
+    twitter: 'ken_wheeler',
+    github: 'kenwheeler',
+  },
+  {
+    imageUrl: '/static/speakers/speaker-2',
+    imageFormat: 'jpg',
+    name: 'Kim Brandwijk',
+    bio: 'Author of SupergraphQL',
+    twitter: undefined,
+    github: 'kbrandwijk',
+  },
+  {
+    imageUrl: '/static/speakers/speaker-3',
+    imageFormat: 'jpg',
+    name: 'Sara Vieira',
+    bio: 'Frontend Engineer at YLD.io',
+    twitter: 'NikkitaFTW',
+    github: 'SaraVieira',
+  },
+  {
+    imageUrl: '/static/speakers/speaker-4',
+    imageFormat: 'jpg',
+    name: 'Martijn Walraven',
+    bio: 'Technical Product Manager at Apollo',
+    twitter: 'martijnwalraven',
+    github: 'martijnwalraven',
+  },
+  {
+    imageUrl: '/static/speakers/speaker-5',
+    imageFormat: 'jpg',
+    name: 'Manjula Dabe',
+    bio: 'Senior Engineer at Bookmyshow',
+    twitter: 'manjula_dube',
+    github: 'manjula91',
+  },
+  {
+    imageUrl: '/static/speakers/speaker-6',
+    imageFormat: 'jpg',
+    name: 'Ivan Goncharov',
+    bio: 'GraphQL & OpenAPI/Swagger consultant at APIsGuru',
+    twitter: undefined,
+    github: 'IvanGoncharov',
+  },
+]
